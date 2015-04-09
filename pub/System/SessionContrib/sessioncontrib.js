597f751da8d4002dbb3f5a303c758a08c47b543c
badakhshan <badakhshan@modell-aachen.de>
2015-04-09 16:49:02 +0200

    draft of session history on sesion panel
    ticket: none

--- pub/System/SessionContrib/sessioncontrib.js
+++ pub/System/SessionContrib/sessioncontrib.js
@@ -2,4 +2,10 @@ jQuery(function($){
 
 	$('.qw-sessionbar').offcanvas({action: "open"});
 
+	 	$('.taskdetail-list li').on('click', function(e) {
+ 		e.preventDefault();
+ 		$(this).addClass('active').siblings().removeClass('active'); // for different background on clicked menu item
+	});
+
+
 });
