6993080a8dcb7187ace51579301cbb8710543a68
badakhshan <badakhshan@modell-aachen.de>
2015-04-15 14:27:29 +0200

    Added the History panels of MinutesAppContrib
    Ticket: none

--- pub/System/SessionContrib/sessioncontrib.js
+++ pub/System/SessionContrib/sessioncontrib.js
@@ -1,8 +1,15 @@
 jQuery(function($){ 
 
-	$('.qw-sessionbar').offcanvas({action: "open"});
+	/*for showing the right panel between 
+	MinutesViewTwmplate and WebHomeViewTemplate */
+	
+    var bar = $('.dataBar').attr("bar");
+	if (bar) {
+		$(bar).offcanvas({action: "open"});
+	};
+	
 
-	 	$('.taskdetail-list li').on('click', function(e) {
+	$('.taskdetail-list li').on('click', function(e) {
  		e.preventDefault();
  		$(this).addClass('active').siblings().removeClass('active'); // for different background on clicked menu item
 	});
