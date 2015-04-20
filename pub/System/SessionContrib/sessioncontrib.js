f0d2a80cf10369e67e30df428d5853d5d1f69e4b
badakhshan <badakhshan@modell-aachen.de>
2015-04-20 11:31:09 +0200

    next meetintg date has emplemented.
    Ticket: none

--- pub/System/SessionContrib/sessioncontrib.js
+++ pub/System/SessionContrib/sessioncontrib.js
@@ -1,8 +1,42 @@
 jQuery(function($){ 
 
+var d = $("#day").html();
+var x = 0;
+
+if (d === "Saturday") { 
+    var x = 6;
+};
+if (d === "Sunday") { 
+    var x = 0;
+};
+if (d === "Monday") { 
+    var x = 1;
+};
+
+if (d === "Tuesday") { 
+    var x = 2;
+};
+if (d === "Wednesday") { 
+    var x = 3;
+};
+if (d === "Thursday") { 
+    var x = 4;
+};
+if (d === "Friday") { 
+    var x = 5;
+};
+var b = new Date();
+var n = b.getDay();
+var c = n - x;
+var newDate = new Date();
+var m = moment(newDate.setDate(b.getDate() - c)).format("YYYY-MM-DD").toString();
+$("#date").text(m);
+
+
+
 	/*for showing the right panel between 
 	MinutesViewTwmplate and WebHomeViewTemplate */
-	
+
     var bar = $('.dataBar').attr("bar");
 	if (bar) {
 		$(bar).offcanvas({action: "open"});
