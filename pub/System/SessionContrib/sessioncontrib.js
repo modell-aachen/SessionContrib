12c4fdbd1b242c2d32381480c062b708e943392f
badakhshan <badakhshan@modell-aachen.de>
2015-04-22 14:49:59 +0200

    did the change on title of filter on TasksAPIPlugin
    Ticket: none

--- pub/System/SessionContrib/sessioncontrib.js
+++ pub/System/SessionContrib/sessioncontrib.js
@@ -1,38 +1,41 @@
 jQuery(function($){ 
 
-var d = $("#day").html();
-var x = 0;
+	/*for showing the upcomming meeting on session data panel */
+	var d = $("#day").text();
+	var x = 0;
 
-if (d === "Saturday") { 
-    var x = 6;
-};
-if (d === "Sunday") { 
-    var x = 0;
-};
-if (d === "Monday") { 
-    var x = 1;
-};
+	if (d === "Saturday") { 
+	    var x = 6;
+	};
+	if (d === "Sunday") { 
+	    var x = 0;
+	};
+	if (d === "Monday") { 
+	    var x = 1;
+	};
 
-if (d === "Tuesday") { 
-    var x = 2;
-};
-if (d === "Wednesday") { 
-    var x = 3;
-};
-if (d === "Thursday") { 
-    var x = 4;
-};
-if (d === "Friday") { 
-    var x = 5;
-};
-var b = new Date();
-var n = b.getDay();
-var c = n - x;
-var newDate = new Date();
-var m = moment(newDate.setDate(b.getDate() - c)).format("YYYY-MM-DD").toString();
-$("#date").text(m);
+	if (d === "Tuesday") { 
+	    var x = 2;
+	};
+	if (d === "Wednesday") { 
+	    var x = 3;
+	};
+	if (d === "Thursday") { 
+	    var x = 4;
+	};
+	if (d === "Friday") { 
+	    var x = 5;
+	};
+	var b = new Date();
+	var n = b.getDay();
+	var c = n - x;
+	var newDate = new Date();
+	var m = moment(newDate.setDate(b.getDate() - c)).format("YYYY-MM-DD").toString();
+	$("#date").text(m);
 
-
+	/*for making the "tasks" from TasksAPIPlugin to Agenda */
+	$(".qw-extra .title").text("Agenda,"); 
+	
 
 	/*for showing the right panel between 
 	MinutesViewTwmplate and WebHomeViewTemplate */
